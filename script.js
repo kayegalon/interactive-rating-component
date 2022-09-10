8f4aefc9e40ea7e8cc877a452504bac4ee010c4b
kayegalon <110833386+kayegalon@users.noreply.github.com>
2022-09-10 22:17:20 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+/* 
+-edit code so that when a rating is selected,
+and user selects another, previous selected is dismissed
+*/
+
+const ratingCard = document.getElementById("rating");
+const rate1 = document.getElementById("one");
+const rate2 = document.getElementById("two");
+const rate3 = document.getElementById("three");
+const rate4 = document.getElementById("four");
+const rate5 = document.getElementById("five");
+const resultText = document.getElementById("result");
+const submitBtn = document.getElementById("submit");
+
+submitBtn.addEventListener("click", function() {
+	ratingCard.style.display = "none";
+})
+
+rate1.addEventListener("click", function() {
+	clicked(rate1, rate2, rate3, rate4, rate5);
+})
+
+rate2.addEventListener("click", function() {
+	clicked(rate2, rate1, rate3, rate4, rate5);
+})
+
+rate3.addEventListener("click", function() {
+	clicked(rate3, rate1, rate2, rate4, rate5);
+})
+
+rate4.addEventListener("click", function() {
+	clicked(rate4, rate1, rate2, rate3, rate5);
+})
+
+rate5.addEventListener("click", function() {
+	clicked(rate5, rate1, rate2, rate3, rate4);
+})
+
+function clicked(rate, others1, others2, others3, others4) {
+	rate.style.borderColor = "hsl(25, 97%, 53%)";
+	rate.style.backgroundColor = "hsl(25, 97%, 53%)";
+	rate.style.color = "hsl(0, 0%, 100%)";
+	resultText.textContent = `You selected ${rate.textContent} out of 5`;
+	
+	const color = "hsl(213, 19%, 18%)";
+	others1.style.borderColor = others2.style.borderColor = others3.style.borderColor = others4.style.borderColor = color;
+	others1.style.backgroundColor = others2.style.backgroundColor = others3.style.backgroundColor = others4.style.backgroundColor = color;
+	others1.style.color = others2.style.color = others3.style.color = others4.style.color = others5.style.color = color;
+}
